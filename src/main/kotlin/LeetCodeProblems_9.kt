d6c5c5bb2ec5a87cb09632b264446a2f39bfbdf0
poran <poran.cse@gmail.com>
2022-07-30 01:40:33 +0600

    solved more optimize way palindrome problem

--- src/main/kotlin/LeetCodeProblems_9.kt
+++ src/main/kotlin/LeetCodeProblems_9.kt
@@ -2,8 +2,9 @@ fun main(args: Array<String>) {
     println("Hello World!")
 
     println("isPalindrome ${isPalindrome(123)}")
-    println("isPalindrome1 ${isPalindrome1(121)}")
+    println("isPalindrome1 ${isPalindrome1(12321)}")
 
+    println("isPalindromeWithoutConvertString ${isPalindromeWithoutConvertString(12321)}")
 
 }
 
@@ -20,4 +21,25 @@ fun isPalindrome1(x: Int): Boolean {
         reverseString += xStr[i]
     }
     return xStr == reverseString
+}
+
+
+//optimize solution for check palindrome number
+fun isPalindromeWithoutConvertString(x: Int): Boolean {
+    // If x is a negative number it is not a palindrome
+    // If x % 10 = 0, in order for it to be a palindrome the first digit should also be 0
+    if (x < 0 || (x != 0 && x%10 == 0))  return false
+
+    var  reverseNum = 0
+    var xTemp = x
+
+    while (xTemp > reverseNum) {
+        reverseNum = reverseNum * 10 + (xTemp % 10) //xTemp =  123 reverseNum = 0 * 10 + (123 % 10) = 0 + 3
+        xTemp /= 10
+    }
+    // If x is equal to reversed number then it is a palindrome
+    // If x has odd number of digits, dicard the middle digit before comparing with x
+    // Example, if x = 23132, at the end of for loop x = 23 and reversedNum = 231
+    // So, reversedNum/10 = 23, which is equal to x
+    return xTemp == reverseNum || (reverseNum / 10 == xTemp)
 }
