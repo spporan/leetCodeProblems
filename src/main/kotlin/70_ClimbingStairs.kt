d73fea5f16752dff09c5fd9a96efab3429142ba6
poran <poran.cse@gmail.com>
2022-08-20 11:54:34 +0600

    Solved 70 Climbing Stairs  using tabulation    problem link: https://leetcode.com/problems/climbing-stairs/
      Complexity:
    
       here,
        n = input number n
    
      time: O(n * 2) = O(n)
      space: O(n)

--- src/main/kotlin/70_ClimbingStairs.kt
+++ src/main/kotlin/70_ClimbingStairs.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("num of way ${climbStairs(45)}")
+    println("num of way ${climbStairsDP(45)}")
 }
 
 /**
@@ -29,4 +29,53 @@ fun climbStairs(n: Int): Int {
     }
 
     return table[n]
+}
+
+fun climbStairsUsingMemo(
+    targetValue: Int,
+    init: Int = 0,
+    memo: HashMap<Int, Int> = hashMapOf()
+): Int {
+    if (memo.containsKey(init)) return memo.getValue(init)
+    if (init == targetValue) return 1
+
+    if (init > targetValue) return 0
+
+    val result = climbStairsUsingBruteForce(targetValue, init + 1) +
+            climbStairsUsingBruteForce(targetValue, init + 2)
+    memo[init] = result
+
+    return memo.getValue(init)
+
+}
+
+fun climbStairsUsingBruteForce(targetValue: Int, init: Int = 0): Int {
+
+    if (init == targetValue) return 1
+
+    if (init > targetValue) return 0
+    return climbStairsUsingBruteForce(targetValue, init + 1) +
+            climbStairsUsingBruteForce(targetValue, init + 2)
+
+}
+
+/**
+ * this loop run n-1 times
+ *
+ * complexity
+ *
+ *  time: O(n-1) = O(n)
+ *  space: O(n-1) = O(n)
+ */
+fun climbStairsDP(n: Int): Int {
+
+    var one = 1
+    var two = 1
+
+    for (i in 1 until n) {
+        val temp =  one
+        one += two
+        two = temp
+    }
+    return one
 }
