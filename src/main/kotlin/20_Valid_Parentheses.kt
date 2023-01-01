60e32d5a2f78e0caac5f2f6f389a2ff5188c63d4
poran <poran.cse@gmail.com>
2023-01-01 23:01:39 +0600

    20. Valid parentheses
    https://leetcode.com/problems/valid-parentheses/description/

--- src/main/kotlin/20_Valid_Parentheses.kt
+++ src/main/kotlin/20_Valid_Parentheses.kt
@@ -1,11 +1,11 @@
 import java.util.Stack
 
 fun main() {
-    println("is valid ${isValid("()[]{}")}")
+    println("is valid ${isValid("()[{}")}")
 }
 
 fun isValid(s: String): Boolean {
-    val stack: Stack<Char> = Stack()
+    val stack: MyCustomStack<Char> = MyCustomStack()
 
     for (char in s.toCharArray()) {
         if(char == ')' && stack.isNotEmpty() && stack.peek() == '(') {
@@ -21,4 +21,30 @@ fun isValid(s: String): Boolean {
         }
     }
     return stack.isEmpty()
+}
+
+internal class MyCustomStack<T>() {
+    private val stack: MutableList<T> = mutableListOf()
+
+    fun clear() {
+        stack.clear()
+    }
+
+    fun isEmpty() = stack.isEmpty()
+
+    fun isNotEmpty() = stack.isNotEmpty()
+
+    fun pop() {
+        if (stack.isEmpty()) throw Exception("Empty stack")
+        stack.removeAt(stack.size - 1)
+    }
+
+    fun push(value: T) {
+        stack.add(value)
+    }
+
+    fun peek(): T {
+        if (stack.isEmpty()) throw Exception("Empty stack")
+        return stack[stack.size - 1]
+    }
 }
